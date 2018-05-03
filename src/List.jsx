019a6d56a8e11dc5f8fe2344cc0517d06dc0961e
Janssen Rodrigues <faker.rdc@hotmail.com>
2018-05-03 19:21:09 -0300

    Addind function to change state of tasks

--- src/List.jsx
+++ src/List.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import "./css/index.css";
 
 class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tasks: {
-        correr: { done: false },
-        pular: { done: false },
-        comer: { done: false }
+        correr: false,
+        pular: false,
+        comer: false
       }
     };
   }
 
   addTask(e) {
     e.preventDefault();
+    if (this.refs.newTask.value === "") {
+      return;
+    }
     let newTask = this.refs.newTask.value;
     let state = this.state.tasks;
-    state[newTask] = { done: false };
+    state[newTask] = false;
 
     this.setState({
       tasks: state
@@ -29,19 +33,34 @@ class List extends React.Component {
     this.setState({
       tasks: tasks
     });
+  }
+  changeStatus(task) {
+    let state = this.state.tasks;
+    let newValue = (state[task] = !state[task]);
+    this.setState({
+      tasks: newValue
+    });
     console.log(this.state.tasks);
   }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <form onSubmit={this.addTask.bind(this)}>
-          <input type="text" ref="newTask" />
+          <input
+            type="text"
+            placeholder="Digite uma nova tarefa"
+            ref="newTask"
+          />
           <input type="submit" value="Add" />
         </form>
         <ul>
           {Object.keys(this.state.tasks).map(task => (
-            <li ref="task">
+            <li
+              style={{ textDecoration: task.value ? "underline" : "none" }}
+              ref="task"
+              onClick={this.changeStatus.bind(this, task)}
+            >
               {task}
               <button onClick={this.removeTask.bind(this, task)}>
                 DELETAR
